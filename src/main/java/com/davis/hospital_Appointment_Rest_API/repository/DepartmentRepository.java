e7540c3301b32eba7fb1ba59225838e5587399a4
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-05 22:07:31 +0300

    added Billing,Department and Prescription repository

---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.davis.hospital_Appointment_Rest_API.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davis.hospital_Appointment_Rest_API.model.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, String>{
+
+}
