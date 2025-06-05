45937f4b162bf58372f39047083cf7e1ca36684e
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-05 17:01:33 +0300

    added Notification class

---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/model/Notification.java
@@ -0,0 +1,100 @@
+package com.davis.hospital_Appointment_Rest_API.model;
+
+import java.util.Date;
+
+public class Notification {
+	private Long id;
+	private User user;
+	private String message;
+	private String messageType;
+	private boolean isRead;
+	private Date createdOn;
+	/**
+	 * @param user
+	 * @param message
+	 * @param messageType
+	 * @param createdOn
+	 */
+	public Notification(User user, String message, String messageType, Date createdOn) {
+		this.user = user;
+		this.message = message;
+		this.messageType = messageType;
+		this.createdOn = createdOn;
+	}
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the messageType
+	 */
+	public String getMessageType() {
+		return messageType;
+	}
+	/**
+	 * @param messageType the messageType to set
+	 */
+	public void setMessageType(String messageType) {
+		this.messageType = messageType;
+	}
+	/**
+	 * @return the isRead
+	 */
+	public boolean isRead() {
+		return isRead;
+	}
+	/**
+	 * @param isRead the isRead to set
+	 */
+	public void setRead(boolean isRead) {
+		this.isRead = isRead;
+	}
+	/**
+	 * @return the createdOn
+	 */
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	/**
+	 * @param createdOn the createdOn to set
+	 */
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	
+	
+	
+
+}
