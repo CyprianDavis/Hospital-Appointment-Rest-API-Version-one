b68060dbd5b993dd445e7ecb93ae7cfcaaca46e6
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-05 22:24:32 +0300

    modified PatientRepository

--- src/main/java/com/davis/hospital_Appointment_Rest_API/repository/PatientRepository.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/PatientRepository.java
@@ -1,5 +1,9 @@
 package com.davis.hospital_Appointment_Rest_API.repository;
 
-public interface PatientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davis.hospital_Appointment_Rest_API.model.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, String>{
 
 }
