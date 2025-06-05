b590e3abea920ea9e3a7fda44c435ef1ca1a2bbf
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-05 22:41:02 +0300

    added ProjectSecurityConfig

---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/config/ProjectSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.davis.hospital_Appointment_Rest_API.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+	@Bean
+	SecurityFilterChain deSecurityFilterChain(HttpSecurity http) throws Exception{
+		http.csrf(csrfConfig -> csrfConfig.disable());
+				
+		http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		return http.build();
+		
+	}
+
+}
