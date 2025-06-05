a6abeec27a56abea6d920661b6b377ec54dd3788
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-05 21:51:34 +0300

    added Repository Interfaces for User,Admin,Doctor and Patient

---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.davis.hospital_Appointment_Rest_API.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davis.hospital_Appointment_Rest_API.model.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, String>{
+
+}
---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.davis.hospital_Appointment_Rest_API.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davis.hospital_Appointment_Rest_API.model.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+	
+
+}
