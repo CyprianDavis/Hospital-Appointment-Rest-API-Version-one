a1f65b1c61d7fd5d91977b5d9a5de19180dd707e
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-07 15:57:02 +0300

    added Repository annotation in AppointmentRepository

--- src/main/java/com/davis/hospital_Appointment_Rest_API/repository/AppointmentRepository.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/AppointmentRepository.java
@@ -1,9 +1,12 @@
 package com.davis.hospital_Appointment_Rest_API.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.davis.hospital_Appointment_Rest_API.model.Appointment;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
 
+	
 }
