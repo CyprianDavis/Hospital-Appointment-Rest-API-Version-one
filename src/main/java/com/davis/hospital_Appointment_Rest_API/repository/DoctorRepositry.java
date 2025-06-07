1d47ef124607a23a2ef6c24ef12bbee7482f2d53
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-07 16:11:37 +0300

    Added custom methods in DoctorRepositry

--- src/main/java/com/davis/hospital_Appointment_Rest_API/repository/DoctorRepositry.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/DoctorRepositry.java
@@ -1,9 +1,18 @@
 package com.davis.hospital_Appointment_Rest_API.repository;
 
+import java.util.List;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.davis.hospital_Appointment_Rest_API.model.Doctor;
 
+
 public interface DoctorRepositry extends JpaRepository<Doctor, String>{
+	
+	
+	List<Doctor> findBySpecialization(String specialization);
+	
+	List<Doctor> findBySurNameOrGivenNameOrOtherName(String surName, String givenName, String otherName);
 
 }
