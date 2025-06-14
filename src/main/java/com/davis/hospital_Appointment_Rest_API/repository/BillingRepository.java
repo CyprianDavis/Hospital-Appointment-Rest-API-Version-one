b1ad22ea30594c130fd3fa678c5ab44b91fa295c
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-14 23:36:16 +0300

    added searchByPatientName in BillingRepository

--- src/main/java/com/davis/hospital_Appointment_Rest_API/repository/BillingRepository.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/BillingRepository.java
@@ -1,11 +1,22 @@
 package com.davis.hospital_Appointment_Rest_API.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.davis.hospital_Appointment_Rest_API.model.Billing;
 
 @Repository
 public interface BillingRepository extends JpaRepository<Billing, Long> {
+	
+	@Query("SELECT b FROM Billing b WHERE "
+			+  "LOWER(d.surName) LIKE LOWER(CONCAT('%', :name, '%')) OR " +
+	           "LOWER(d.givenName) LIKE LOWER(CONCAT('%', :name, '%')) OR " +
+	           "LOWER(d.otherName) LIKE LOWER(CONCAT('%', :name, '%'))")
+	List<Billing> searchByPatientName(@Param("name") String name);
+	
 
 }
