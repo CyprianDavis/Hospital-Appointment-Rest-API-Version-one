9ebac7ad87f840184e94b4dede6626642e30a3a7
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-14 23:29:53 +0300

    added BillingService

---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/service/BillingService.java
@@ -0,0 +1,11 @@
+package com.davis.hospital_Appointment_Rest_API.service;
+
+import com.davis.hospital_Appointment_Rest_API.model.Billing;
+/**
+ * @author CYPRIAN DAVIS
+ */
+public interface BillingService extends Service<Billing, Long>{
+	
+	
+
+}
