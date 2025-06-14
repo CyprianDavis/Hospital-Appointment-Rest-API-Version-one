ba57860c07a14ae6181b3752c8b521efd9dd14da
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-14 23:37:34 +0300

    added searchByPatientName

--- src/main/java/com/davis/hospital_Appointment_Rest_API/service/BillingService.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/service/BillingService.java
@@ -1,11 +1,13 @@
 package com.davis.hospital_Appointment_Rest_API.service;
 
+import java.util.List;
+
 import com.davis.hospital_Appointment_Rest_API.model.Billing;
 /**
  * @author CYPRIAN DAVIS
  */
 public interface BillingService extends Service<Billing, Long>{
-	
+	List<Billing> searchByPatientName(String name);
 	
 
 }
