584d2f9279cdb5acd7ad368fc4b99a1279dcef02
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-16 13:59:32 +0300

    added findByUserName method in NotificationRepository

--- src/main/java/com/davis/hospital_Appointment_Rest_API/repository/NotificationRepository.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/repository/NotificationRepository.java
@@ -1,9 +1,18 @@
 package com.davis.hospital_Appointment_Rest_API.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.davis.hospital_Appointment_Rest_API.model.Notification;
-
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
+	
+	@Query("SELECT n FROM Notification n WHERE "
+			+ "n.user.userName =:userName")
+	Optional<Notification> findByUserName(@Param("userName")String userName);
 
 }
