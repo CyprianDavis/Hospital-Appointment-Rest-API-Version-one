955900890f1be83b2ce14120798872fb226539cc
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-16 13:11:43 +0300

    implemented AuthorityServiceImp

---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/service/imp/AuthorityServieImp.java
@@ -0,0 +1,36 @@
+package com.davis.hospital_Appointment_Rest_API.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.davis.hospital_Appointment_Rest_API.model.Authority;
+import com.davis.hospital_Appointment_Rest_API.repository.AuthorityRepository;
+import com.davis.hospital_Appointment_Rest_API.service.AuthorityService;
+/**
+ * @author CYPRIAN DAVIS
+ */
+
+public class AuthorityServieImp implements AuthorityService {
+	@Autowired
+	private AuthorityRepository authorityRepository;
+
+	@Override
+	public List<Authority> findAll() {
+		// TODO Auto-generated method stub
+		return authorityRepository.findAll();
+	}
+
+	@Override
+	public Authority save(Authority authority) {
+		// TODO Auto-generated method stub
+		return authorityRepository.save(authority);
+	}
+
+	@Override
+	public Authority findByName(String name) {
+		// TODO Auto-generated method stub
+		return authorityRepository.findByNameIgnoreCase(name);
+	}
+
+}
