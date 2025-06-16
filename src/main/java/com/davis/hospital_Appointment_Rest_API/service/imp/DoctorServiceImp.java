59a3a95086d26262f93b8648b5a9248511e23ded
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-16 12:06:11 +0300

    Implemented DoctorServieImp

---
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/service/imp/DoctorServiceImp.java
@@ -0,0 +1,45 @@
+package com.davis.hospital_Appointment_Rest_API.service.imp;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.davis.hospital_Appointment_Rest_API.model.Doctor;
+import com.davis.hospital_Appointment_Rest_API.repository.DoctorRepository;
+import com.davis.hospital_Appointment_Rest_API.service.DoctorService;
+import com.davis.hospital_Appointment_Rest_API.service.Service;
+
+
+public class DoctorServiceImp implements DoctorService,Service<Doctor>{
+	@Autowired
+	private DoctorRepository doctorRepository;
+
+	@Override
+	public List<Doctor> findAll() {
+		// TODO Auto-generated method stub
+		return doctorRepository.findAll();
+	}
+
+	@Override
+	public Doctor save(Doctor doctor) {
+		// TODO Auto-generated method stub
+		return doctorRepository.save(doctor);
+	}
+
+	@Override
+	public List<Doctor> searchBySpecialization(String specialization) {
+		// TODO Auto-generated method stub
+		return doctorRepository.findBySpecialization(specialization);
+	}
+
+	@Override
+	public List<Doctor> searchByNames(String names) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		return null;
+	}
+
+}
