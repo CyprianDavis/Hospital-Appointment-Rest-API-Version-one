ba3ba94a6b32dc23529c13b7997f6ab3bb11695c
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-06-16 14:13:09 +0300

    modified ProjectSecurityConfig added passwordEncoder

--- src/main/java/com/davis/hospital_Appointment_Rest_API/config/ProjectSecurityConfig.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/config/ProjectSecurityConfig.java
@@ -5,6 +5,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +18,12 @@ public class ProjectSecurityConfig {
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
+		
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		
 	}
 
