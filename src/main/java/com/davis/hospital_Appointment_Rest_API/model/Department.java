f065891d86fd64b1d604352fd1c32434a298dc50
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-07-19 21:37:30 +0300

    modified Department Entity

--- src/main/java/com/davis/hospital_Appointment_Rest_API/model/Department.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/model/Department.java
@@ -7,8 +7,11 @@ import java.util.Set;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -70,7 +73,8 @@ public class Department {
     @UpdateTimestamp
     @Column(name="lastUpdated")
     private LocalDateTime updatedOn;
-    @OneToMany(mappedBy = "department")
+    @JsonIgnore
+    @OneToMany(mappedBy = "department", fetch =FetchType.LAZY )
     private Set<Doctor>doctors = new HashSet<>();
     
     /**
