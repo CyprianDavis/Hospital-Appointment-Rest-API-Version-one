f20fbc9b9f23c2b189a2b3b69e95c54e3eb0f151
CYPRIAN DAVIS <cypriandavis25@gmail.com>
2025-07-22 21:06:23 +0300

    added getPatients method in patientController

--- src/main/java/com/davis/hospital_Appointment_Rest_API/controller/PatientController.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/controller/PatientController.java
@@ -1,9 +1,16 @@
 package com.davis.hospital_Appointment_Rest_API.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davis.hospital_Appointment_Rest_API.dto.PatientDto;
 import com.davis.hospital_Appointment_Rest_API.service.imp.PatientServiceImp;
+import com.davis.hospital_Appointment_Rest_API.utils.ApiResponse;
 
 @RestController
 @RequestMapping("/api/patient")
@@ -13,7 +20,16 @@ public class PatientController {
 	public PatientController(PatientServiceImp patientServiceImp) {
 		this.patientServiceImp = patientServiceImp;
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN', 'DOCTOR')")
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<PatientDto>>> getPatients(){
+		List<PatientDto> patients = patientServiceImp.findAllPatients();
+		String message = patients.isEmpty() ? 
+	            "No patients found" : 
+	            "Patients retrieved successfully";
+		return ResponseEntity.ok(ApiResponse.success(message,patients));
+	}
 	
 	
 	
