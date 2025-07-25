995598b23efe7d8bfe77f3c0816e16246cccef4a
SSEREMBA CYPRIAN DAVIS <128586128+CyprianDavis@users.noreply.github.com>
2025-07-26 00:20:39 +0300

    changed DoctorBreaks entity to an Embeddable object

--- src/main/java/com/davis/hospital_Appointment_Rest_API/model/DoctorBreaks.java
+++ src/main/java/com/davis/hospital_Appointment_Rest_API/model/DoctorBreaks.java
@@ -2,99 +2,78 @@ package com.davis.hospital_Appointment_Rest_API.model;
 
 import java.time.LocalTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.Embeddable;
 
-@Entity
-@Table(name = "Doctor_Breaks")
+/**
+ * Represents a break period in a doctor's schedule.
+ * This embeddable class can be included in DoctorSchedule entity.
+ */
+@Embeddable
 public class DoctorBreaks {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private DoctorSchedule doctorSchedule;
-	private String breakType;
-	private LocalTime sartTime;
-	private LocalTime endTime;
-	/**
-	 * @param id
-	 * @param doctorSchedule
-	 * @param breakType
-	 * @param sartTime
-	 * @param endTime
-	 */
-	public DoctorBreaks(Long id, DoctorSchedule doctorSchedule, String breakType, LocalTime sartTime,
-			LocalTime endTime) {
-		this.id = id;
-		this.doctorSchedule = doctorSchedule;
-		this.breakType = breakType;
-		this.sartTime = sartTime;
-		this.endTime = endTime;
-	}
-	/**
-	 * @return the id
-	 */
-	public Long getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
-	/**
-	 * @return the doctorSchedule
-	 */
-	public DoctorSchedule getDoctorSchedule() {
-		return doctorSchedule;
-	}
-	/**
-	 * @param doctorSchedule the doctorSchedule to set
-	 */
-	public void setDoctorSchedule(DoctorSchedule doctorSchedule) {
-		this.doctorSchedule = doctorSchedule;
-	}
-	/**
-	 * @return the breakType
-	 */
-	public String getBreakType() {
-		return breakType;
-	}
-	/**
-	 * @param breakType the breakType to set
-	 */
-	public void setBreakType(String breakType) {
-		this.breakType = breakType;
-	}
-	/**
-	 * @return the sartTime
-	 */
-	public LocalTime getSartTime() {
-		return sartTime;
-	}
-	/**
-	 * @param sartTime the sartTime to set
-	 */
-	public void setSartTime(LocalTime sartTime) {
-		this.sartTime = sartTime;
-	}
-	/**
-	 * @return the endTime
-	 */
-	public LocalTime getEndTime() {
-		return endTime;
-	}
-	/**
-	 * @param endTime the endTime to set
-	 */
-	public void setEndTime(LocalTime endTime) {
-		this.endTime = endTime;
-	}
-	
+    
+    private String breakType;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
-	
-	
+    /**
+     * Default constructor
+     */
+    public DoctorBreaks() {
+    }
+
+    /**
+     * Constructs a DoctorBreaks with all fields
+     * @param breakType The type of break (e.g., "Lunch", "Meeting")
+     * @param startTime The start time of the break
+     * @param endTime The end time of the break
+     */
+    public DoctorBreaks(String breakType, LocalTime startTime, LocalTime endTime) {
+        this.breakType = breakType;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /**
+     * @return the breakType
+     */
+    public String getBreakType() {
+        return breakType;
+    }
+
+    /**
+     * @param breakType the breakType to set
+     */
+    public void setBreakType(String breakType) {
+        this.breakType = breakType;
+    }
+
+    /**
+     * @return the startTime
+     */
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * @param startTime the startTime to set
+     */
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    /**
+     * @return the endTime
+     */
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * @param endTime the endTime to set
+     */
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+  
 }
